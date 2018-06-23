bbc5b5188d8669c2a545db78dba3e9f6f8560a1d
jerry <urbrioche@gmail.com>
2018-06-23 14:00:28 +0800

    complete

---
+++ TennisGame.py
@@ -0,0 +1,59 @@
+class TennisGame(object):
+
+    def __init__(self, first_player_name, second_player_name):
+        self.first_player_name = first_player_name
+        self.second_player_name = second_player_name
+        self.second_player_score_times = 0
+        self.first_player_score_times = 0
+        self.score_lookup = {
+            0: 'love',
+            1: 'fifteen',
+            2: 'thirty',
+            3: 'forty',
+        }
+
+    @property
+    def score(self):
+        if self.is_score_different():
+            if self.is_ready_for_win():
+                return self.adv_state()
+            return self.normal_score()
+
+        if self.is_deuce():
+            return 'deuce'
+        return self.same_score()
+
+    def same_score(self):
+        return self.score_lookup[self.first_player_score_times] + ' all';
+
+    def is_deuce(self):
+        times_ = self.first_player_score_times >= 3
+        return times_
+
+    def adv_state(self):
+        return self.adv_player() + (' Adv' if self.is_adv() else ' Win')
+
+    def is_score_different(self):
+        times = self.first_player_score_times != self.second_player_score_times
+        return times
+
+    def is_ready_for_win(self):
+        times_ = self.first_player_score_times > 3 or self.second_player_score_times > 3
+        return times_
+
+    def normal_score(self):
+        return self.score_lookup[self.first_player_score_times] + ' ' + self.score_lookup[
+            self.second_player_score_times]
+
+    def is_adv(self):
+        return abs(self.first_player_score_times - self.second_player_score_times) == 1
+
+    def adv_player(self):
+        adv_player = self.first_player_name if self.first_player_score_times > self.second_player_score_times else self.second_player_name
+        return adv_player
+
+    def first_player_score(self):
+        self.first_player_score_times += 1
+
+    def second_player_score(self):
+        self.second_player_score_times += 1
---
+++ TennisGameTest.py
@@ -0,0 +1,82 @@
+import unittest
+
+from TennisGame import TennisGame
+
+
+class TennisGameTest(unittest.TestCase):
+    def setUp(self):
+        self.game = TennisGame('Joey','Tom')
+
+    def test_love_all(self):
+        self.score_should_be()
+
+    def test_fifteen_love(self):
+        self.game.first_player_score()
+        self.score_should_be('fifteen love')
+
+    def test_thirty_love(self):
+        self.given_first_player_score_times(2)
+        self.score_should_be('thirty love')
+
+    def test_forty_love(self):
+        self.given_first_player_score_times(3)
+        self.score_should_be('forty love')
+
+    def test_love_fifteen(self):
+        self.game.second_player_score()
+        self.score_should_be('love fifteen')
+
+    def test_love_thirty(self):
+        self.given_second_player_score_times(2)
+        self.score_should_be('love thirty')
+
+    def test_fifteen_all(self):
+        self.game.first_player_score()
+        self.game.second_player_score()
+        self.score_should_be('fifteen all')
+
+    def test_thirty_all(self):
+        self.given_first_player_score_times(2)
+        self.given_second_player_score_times(2)
+        self.score_should_be('thirty all')
+
+    def test_deuce(self):
+        self.given_first_player_score_times(3)
+        self.given_second_player_score_times(3)
+        self.score_should_be('deuce')
+
+    def test_deuce(self):
+        self.given_first_player_score_times(4)
+        self.given_second_player_score_times(4)
+        self.score_should_be('deuce')
+
+    def test_first_player_advantage(self):
+        self.given_first_player_score_times(4)
+        self.given_second_player_score_times(3)
+        self.score_should_be('Joey Adv')
+
+    def test_second_player_advantage(self):
+        self.given_first_player_score_times(3)
+        self.given_second_player_score_times(4)
+        self.score_should_be('Tom Adv')
+
+    def test_second_player_advantage(self):
+        self.given_first_player_score_times(3)
+        self.given_second_player_score_times(5)
+        self.score_should_be('Tom Win')
+
+    def given_second_player_score_times(self, times):
+        for i in range(times):
+            self.game.second_player_score()
+
+    def given_first_player_score_times(self, times):
+        for i in range(times):
+            self.game.first_player_score()
+
+    def score_should_be(self, expected='love all'):
+        score = self.game.score
+        self.assertEqual('%s' % expected, score)
+
+
+if __name__ == '__main__':
+    unittest.main()
